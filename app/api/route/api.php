363eabceb69e0d7717cc62bb6a955f8594b593df
Dongw <picklinguo@gmail.com>
2023-05-10 14:20:06 +0800

    feat: miss路由

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -5,6 +5,16 @@
 
 use think\facade\Route;
 
-Route::get('test', 'TestController/index')->middleware(\app\api\middleware\CheckAuth::class);
+Route::miss(function() {
+    return api_error_404();
+});
+
+ROute::rule('/', 'Index');
 
 Route::post('login', 'WXLoginController/index');
+
+Route::group('test',function () {
+    Route::get('', 'TestController/index');
+})->middleware(\app\api\middleware\CheckAuth::class);
+
+Route::get('test1', 'TestController/test1');
