928dfbdd9c8ce66cdadbf1cc0bdd8b284b511935
Dongw <picklinguo@gmail.com>
2023-05-10 14:38:15 +0800

    style: 补注释

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -5,16 +5,17 @@
 
 use think\facade\Route;
 
+//api miss route
 Route::miss(function() {
     return api_error_404();
 });
-
+// api index route
 ROute::rule('/', 'Index');
-
+// api login route
 Route::post('login', 'WXLoginController/index');
-
+// api test route
 Route::group('test',function () {
     Route::get('', 'TestController/index');
 })->middleware(\app\api\middleware\CheckAuth::class);
-
+// api test1 route
 Route::get('test1', 'TestController/test1');
