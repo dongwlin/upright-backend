2580df3e40b3387f373f7f2eb9a2ce6a454b47b2
Dongw <picklinguo@gmail.com>
2023-05-23 14:25:29 +0800

    feat: 去除user的create接口

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -11,19 +11,21 @@ Route::miss(function() {
 });
 // api index route
 ROute::rule('/', 'Index');
+
 // api login route
 Route::post('login', 'WXLogin/index');
+
 // api test route
 Route::group('test',function () {
-    Route::get('', 'Test/index');
+    Route::get('/', 'test/index');
 })->middleware(\app\api\middleware\CheckAuth::class);
 // api test1 route
-Route::get('test1', 'Test/test1');
+Route::get('test1', 'test/test1');
 
 // user route
 Route::group('user', function () {
-   Route::get('create', 'user/create');
-   Route::post('update', 'user/update');
-   Route::get('read', 'user/read');
-   Route::delete('delete', 'user/delete');
+//   Route::get('create', 'user/create');
+   Route::post('/', 'user/update');
+   Route::get('/', 'user/read');
+   Route::delete('/', 'user/delete');
 })->middleware(\app\api\middleware\CheckAuth::class);
