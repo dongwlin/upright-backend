d334a853e7144a8fa422a37bf0ddd916ff99d907
dongwlin <linsann@163.com>
2024-05-20 11:27:58 +0800

    feat: add solution and test route

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -17,6 +17,7 @@ Route::get('selectLoginStatus', 'test/index');
 Route::get('aggreateDay', 'train/index');
 
 Route::get("trains", "train/queryAll")->middleware(\app\api\middleware\CheckAuth::class);
+Route::get("test", "train/queryAllT")->middleware(\app\api\middleware\CheckAuth::class);
 
 // api login route
 Route::post('login', 'WXLogin/index');
@@ -28,3 +29,10 @@ Route::group('user', function () {
    Route::get('/', 'user/read');
    Route::delete('/', 'user/delete');
 })->middleware(\app\api\middleware\CheckAuth::class);
+
+// solution route
+Route::group('solution', function () {
+    Route::get('/obtainSolution', 'solution/query');
+})->middleware(\app\api\middleware\CheckAuth::class);
+
+Route::get('/try', "solution/query");
